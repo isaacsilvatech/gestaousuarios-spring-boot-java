3b1394f8dde252fe3d239b9db12d6b0ce95fd6d6
IsaacSilva <isaacsilva.info@gmail.com>
2025-12-19 17:07:32 -0300

    [RF][00] - Gestao Usuarios - Primeiro commit

---
+++ src/main/java/br/com/isaacsilva/tech/api/gestaousuarios/GestaoUsuariosApplication.java
@@ -0,0 +1,13 @@
+package br.com.isaacsilva.tech.api.gestaousuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoUsuariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/isaacsilva/tech/api/gestaousuarios/GestaoUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.isaacsilva.tech.api.gestaousuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
